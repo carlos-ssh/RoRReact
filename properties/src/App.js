07d2074a4a8d34ab36b85486fb4e7ea77e44b30d
Carlos <carlos.anezeh@gmail.com>
2022-01-24 18:03:44 -0600

    Fixed issues displaying data from Rails API.

--- properties/src/App.js
+++ properties/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import axios from 'axios';
+import Books from './components/books';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'http://localhost:3000/api/v1/books/';
+
+function getAPIData() {
+  return axios.get(API_URL)
+        .then((response) => response.data)
+        .catch(error => console.log(`Hay un error:${error}`))
+}
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) =>{
+      if (mounted) {
+        setBooks(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello Carlos</h1>
+      <Books books={books} />
     </div>
   );
 }
